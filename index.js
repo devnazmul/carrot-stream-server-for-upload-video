55be6a0ac56df0108f048bab427195165bd99eab
Md Nazmul Islam <md.nazmul.islam1332@gmail.com>
2022-08-12 22:08:47 +0600

    add listenning port

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 
 
+require('dotenv').config()
+
 const app = express()
 
 app.get('/',(req,res)=>{
     res.send(running)
+});
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`app is listening on ${process.env.API_BEGINNING_POINT}:${process.env.PORT}`);
 })
